e76cf41be896b589c77627cfda9973101ab9adc4
JJayet <jonathan@jayet.eu>
2015-03-23 11:26:38 +0100

    Added the status bar class

--- src/ngcordova/statusbar/CordovaStatusBar.hx
+++ src/ngcordova/statusbar/CordovaStatusBar.hx
@@ -1,11 +1,37 @@
 package ngcordova.statusbar;
 
+@:enum
+abstract Styles(Int) {
+	var Default = 0;
+	var LightContent = 1;
+	var BlackTranslucent = 2;
+	var BlackOpaque = 3;
+}
+
+@:enum
+abstract StyleColors(String) {
+	var Black = "black";
+	var DarkGray = "darkGray";
+	var LightGray = "lightGray";
+	var White = "white";
+	var Gray = "gray";
+	var Red = "red";
+	var Green = "green";
+	var Blue = "blue";
+	var Cyan = "cyan";
+	var Yellow = "yellow";
+	var Magenta = "magenta";
+	var Orange = "orange";
+	var Purple = "purple";
+	var Brown = "brown";
+}
+
 @:injectionName("$cordovaStatusbar")
 extern class CordovaStatusBar {
 	public function overlaysWebView(overlay : Bool) : Void;
-	public function style(style : Int) : Void;
+	public function style(style : Styles) : Void;
 	public function styleHex(styleHex : String) : Void;
-	public function styleColor(styleColor : String) : Void;
+	public function styleColor(styleColor : StyleColors) : Void;
 	public function hide() : Void;
 	public function show() : Void;
 	public function isVisible() : Bool;
